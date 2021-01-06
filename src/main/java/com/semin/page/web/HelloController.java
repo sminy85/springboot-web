6d8adcc3c62e4146b2ff1cf2729aed58b643b4c1
sminy85 <smini857@naver.com>
2021-01-06 18:54:49 +0900

    Spring Boot JPA database

--- src/main/java/com/semin/page/web/HelloController.java
+++ src/main/java/com/semin/page/web/HelloController.java
@@ -1,5 +1,7 @@
 package com.semin.page.web;
+import com.semin.page.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,4 +10,11 @@ public class HelloController {
     public String hello() {
         return "hello";
     }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
 }
+
