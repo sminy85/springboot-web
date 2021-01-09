da7a1d645dd7bb12f6e143d55c40783065f80ae4
sminy85 <smini857@naver.com>
2021-01-09 23:57:40 +0900

    mustache 적용

--- src/main/java/com/semin/page/service/posts/PostsService.java
+++ src/main/java/com/semin/page/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.semin.page.service.posts;
 
 import com.semin.page.domain.posts.Posts;
 import com.semin.page.domain.posts.PostsRepository;
+import com.semin.page.web.dto.PostsListResponseDto;
 import com.semin.page.web.dto.PostsResponseDto;
 import com.semin.page.web.dto.PostsSaveRequestDto;
 import com.semin.page.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,8 +35,15 @@ public class PostsService {
 
     public PostsResponseDto findById (Long id) {
         Posts entity = postsRepository.findById(id)
-                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id:"+id));
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id:"+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
